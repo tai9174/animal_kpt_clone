dce421509ca3c9a2bd48541340d2b1cda25b04cb
tai9174 <104898840+tai9174@users.noreply.github.com>
2022-08-25 18:22:37 +0900

    seedデータkpt実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :join_teams, dependent: :destroy
   has_many :join_team_teams, through: :join_teams, source: :team
   has_many :teams, dependent: :destroy
-  has_many :kpts
+  has_many :kpts, dependent: :destroy
   validates :name, presence: true, uniqueness: true, length: {minimum:3, maximum: 20}
   validates :password, presence: true, length: { minimum: 6 }
   # Include default devise modules. Others available are:
